0876130f7a4fd6d84ba6c4660e6ccecf2de08bab
stangeh <matthewshr@gmail.com>
2017-06-17 16:28:39 +0200

    Fixed Allowed Hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'fkq8ys8r9_wxk$t*bh^zw7g_9=f+418wnql-13wm-1x*3b-c@o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<stangeh>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'stangeh.pythonanywhere.com']
 
 
 # Application definition
